37d96cd8d5b0020904bf92d545799f37f0a6f8cd
Michael Stallings <stall84@gmail.com>
2020-09-21 19:01:15 -0400

    using v-if, working on conditionally rendering to dom

--- 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
+++ 04-lists-and-conditionals/begin/vue-heroes/src/components/heroes.vue
@@ -8,9 +8,20 @@
         <header class="card-header">
           <p class="card-header-title">heroes list</p>
         </header>
+        <ul class="list is-hoverable">
+          <li v-for="hero in heroes" :key="hero.id">
+            <a
+              class="list-item"
+              :class="{'is-active': selectedHero === hero }"
+              @click="selectedHero = hero"
+            >
+              <span>{{ hero.firstName }}</span>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-    <div class="columns">
+    <div class="columns" v-if="selectedHero">
       <div class="column is-3">
         <header class="card-header">
           <p class="card-header-title">{{ selectedHero.firstName }}</p>
@@ -19,33 +30,19 @@
           <div class="content">
             <div class="field">
               <label class="label" for="id">id</label>
-              <label class="input" id="id" readonly>
-                {{ selectedHero.id }}
-              </label>
+              <label class="input" id="id" readonly>{{ selectedHero.id }}</label>
             </div>
             <div class="field">
               <label class="label" for="firstName">first name</label>
-              <input
-                class="input"
-                id="firstName"
-                v-model="selectedHero.firstName"
-              />
+              <input class="input" id="firstName" v-model="selectedHero.firstName" />
             </div>
             <div class="field">
               <label class="label" for="lastName">last name</label>
-              <input
-                class="input"
-                id="lastName"
-                v-model="selectedHero.lastName"
-              />
+              <input class="input" id="lastName" v-model="selectedHero.lastName" />
             </div>
             <div class="field">
               <label class="label" for="description">description</label>
-              <input
-                class="input"
-                id="description"
-                v-model="selectedHero.description"
-              />
+              <input class="input" id="description" v-model="selectedHero.description" />
             </div>
           </div>
         </div>
@@ -59,12 +56,7 @@ export default {
   name: 'Heroes',
   data() {
     return {
-      selectedHero: {
-        id: 111,
-        firstName: '...',
-        lastName: '...',
-        description: '...',
-      },
+      selectedHero: undefined,
       heroes: [
         {
           id: 10,
